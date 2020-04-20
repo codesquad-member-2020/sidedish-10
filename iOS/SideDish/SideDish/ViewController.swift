ff5727f6d61bd984f9713ac1dd8db774f0cebaa5
1Consumption <hanseop95@gmail.com>
2020-04-20 21:25:06 +0900

    chore: 프로젝트 생성

---
+++ iOS/SideDish/SideDish/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SideDish
+//
+//  Created by 신한섭 on 2020/04/20.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
