460ec111400216726cb07eacf7ea30e554307041
1Consumption <hanseop95@gmail.com>
2020-04-20 21:46:20 +0900

    chore: Toaster 버전 변경

--- iOS/SideDish/SideDish/ViewController.swift
+++ iOS/SideDish/SideDish/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Toaster
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Toast(text: "ASDASD").show()
     }
 }
 
