0d32680fc961e70fdec215582dc3acd29c364bcb
1Consumption <hanseop95@gmail.com>
2020-04-21 13:18:32 +0900

    [#7] feat: MainMenuViewController 추가

---
+++ iOS/SideDish/SideDish/MainMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenuViewController.swift
+//  SideDish
+//
+//  Created by 신한섭 on 2020/04/21.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
