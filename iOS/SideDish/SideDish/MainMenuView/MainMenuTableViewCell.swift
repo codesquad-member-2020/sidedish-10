54761ab693c591913e8b043fa9ef9f5a5c36cceb
1Consumption <hanseop95@gmail.com>
2020-04-21 15:55:42 +0900

    [#7] feat: 커스텀 셀 적용

---
+++ iOS/SideDish/SideDish/MainMenuView/MainMenuTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuTableViewCell.swift
+//  SideDish
+//
+//  Created by 신한섭 on 2020/04/21.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var menuImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var explanationLabel: UILabel!
+    @IBOutlet weak var originalPriceLabel: UILabel!
+    @IBOutlet weak var specialPriceLabel: UILabel!
+}
--- iOS/SideDish/SideDish/MainMenuView/MainMenuViewController.swift
+++ iOS/SideDish/SideDish/MainMenuView/MainMenuViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
 
+    @IBOutlet weak var mainMenuTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
