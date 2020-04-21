3359f160be60c6d65886482dceb44b385c28259a
1Consumption <hanseop95@gmail.com>
2020-04-21 13:23:23 +0900

    [#7] feat: 네비게이션 바 숨겨도 스와이프 하여 뒤로가기 기능 활성화

--- iOS/SideDish/SideDish/LoginViewController.swift
+++ iOS/SideDish/SideDish/LoginViewController.swift
@@ -12,6 +12,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.interactivePopGestureRecognizer?.delegate = nil
     }
 }
 
