91f1d98436431161bbdb36198cd41e6eeb2e26b8
sangwon21 <junothyme@gmail.com>
2020-04-23 12:08:42 +0900

    [#14] feat: 상세 페이지 컴포넌트 스토리 작성
    why: 상세 페이지 컴포넌트를 살펴보기 위한 스토리를 작성했다.

---
+++ FE/src/stories/DetailPage.stories.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import DetailPage from "../components/DetailPage/DetailPage";
+
+storiesOf("DetailPage", module).addWithJSX("기본 설정", () => <DetailPage />);
