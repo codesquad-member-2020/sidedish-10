ced010e8deb93bdfd662f7c86b8811819e43512e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-24 18:48:40 +0900

    Set SpringBootServletInitializer

--- BE/src/main/java/com/codesquad/sidedish10/Sidedish10Application.java
+++ BE/src/main/java/com/codesquad/sidedish10/Sidedish10Application.java
@@ -4,12 +4,20 @@ import com.codesquad.sidedish10.parser.service.ParseService;
 import java.text.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Sidedish10Application {
+public class Sidedish10Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws ParseException {
 		SpringApplication.run(Sidedish10Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Sidedish10Application.class);
+	}
+
 
 }
