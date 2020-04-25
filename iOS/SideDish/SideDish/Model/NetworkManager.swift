2c0db801cdb197891d0a0a231773804badd17194
1Consumption <hanseop95@gmail.com>
2020-04-26 03:43:17 +0900

    [#30] feat: HTTPResponse가 없는 경우 에러 처리

--- iOS/SideDish/SideDish/Model/NetworkManager.swift
+++ iOS/SideDish/SideDish/Model/NetworkManager.swift
@@ -25,24 +25,27 @@ class NetworkManager: NetworkManageable {
     }
     
     enum NetworkError: Error {
-        case InvalidURL
-        case requestError
-        case InvalidStatusCode(Int)
         case DataEmpty
         case DecodeError
+        case InvalidHTTPResonse
+        case InvalidStatusCode(Int)
+        case InvalidURL
+        case requestError
         
         func message() -> String {
             switch self {
             case .DataEmpty:
                 return "데이터가 비었어요."
+            case .DecodeError:
+                return "응답을 복호화 하는 도중 문제가 발생했어요."
+            case .InvalidHTTPResonse:
+                return "HTTP 응답이 유효하지 않아요."
             case .InvalidStatusCode(let code):
                 return "HTTP 응답 \(code) 에러 발생했어요."
             case .InvalidURL:
                 return "URL이 유효하지 않아요."
             case .requestError:
                 return "요청을 보내는 중에 오류가 발생했어요."
-            case .DecodeError:
-                return "응답을 복호화 하는 도중 문제가 발생했어요."
             }
         }
     }
@@ -61,6 +64,7 @@ class NetworkManager: NetworkManageable {
             }
             
             guard let httpResponse = response as? HTTPURLResponse else {
+                handler(.failure(.InvalidHTTPResonse))
                 return
             }
             
