1b7e964f0049b44f2923368602046dbc6880de17
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-27 19:59:24 +0900

    config: menu domain schema/data added

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO menu (title) VALUES ('든든한반찬');
-INSERT INTO menu (title) VALUES ('국물요리');
-INSERT INTO menu (title) VALUES ('밑반찬');
+INSERT INTO menu (title, eng_name, info) VALUES ('메인반찬', 'main', '한그릇 뚝딱 메인 요리');
+INSERT INTO menu (title, eng_name, info) VALUES ('국물요리', 'soup', '김이 모락모락 국.찌개');
+INSERT INTO menu (title, eng_name, info) VALUES ('밑반찬', 'side', '언제 먹어도 든든한 밑반찬');
 INSERT INTO best_category (title) VALUES ('할인특가세트상품');
 INSERT INTO best_category (title) VALUES ('풍성한고기반찬');
 INSERT INTO best_category (title) VALUES ('편리한반찬세트');
