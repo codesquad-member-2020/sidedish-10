fcc74c6bc534fd437837e75521c67bf2518b0329
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-27 14:07:35 +0900

    [#21] Interceptor and Configurer to be added with login logic

---
+++ BE/src/main/java/com/codesquad/sidedish10/getter/config/WebMvcConfigurerImpl.java
@@ -0,0 +1,34 @@
+package com.codesquad.sidedish10.getter.config;
+
+import com.codesquad.sidedish10.getter.service.JwtService;
+import com.codesquad.sidedish10.getter.interceptor.HttpInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfigurerImpl implements WebMvcConfigurer {
+
+  private final JwtService jwtService;
+
+  public WebMvcConfigurerImpl(JwtService jwtService) {
+    this.jwtService = jwtService;
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST");
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new HttpInterceptor(jwtService))
+        .addPathPatterns("/develop/**")
+        .excludePathPatterns("/login/**");
+  }
+}
