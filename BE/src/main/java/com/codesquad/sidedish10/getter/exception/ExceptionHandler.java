4e4af62842b518748d0d9bc35f527a9df3e9979b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-27 20:02:52 +0900

    chore: reminder for TODOs for refactoring exception

--- BE/src/main/java/com/codesquad/sidedish10/getter/exception/ExceptionHandler.java
+++ BE/src/main/java/com/codesquad/sidedish10/getter/exception/ExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.codesquad.sidedish10.getter.exception;
 
-import com.codesquad.sidedish10.getter.utils.ApiResponse;
+import com.codesquad.sidedish10.getter.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +14,7 @@ public class ExceptionHandler {
   @ResponseBody
   public ApiResponse jwtNotFoundError(JwtMissingException exception) {
     ApiResponse response = exception.returnErrorMessage();
+    //TODO: 아래의 내용을 exception 자체에서 super 메소드를 활용하여 리팩토링하기
     response.setStatusCode(ErrorCodes.FORBIDDEN);
     response.setBody("쿠키에 JWT 넣어서 보내주세요!!");
     return response;
