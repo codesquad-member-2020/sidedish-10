bfca79f195e3745aedc6dab241b0f840c1b0a9ca
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-27 20:00:24 +0900

    feat: returning the map that stores title/color

--- BE/src/main/java/com/codesquad/sidedish10/getter/service/GetterService.java
+++ BE/src/main/java/com/codesquad/sidedish10/getter/service/GetterService.java
@@ -1,19 +1,26 @@
 package com.codesquad.sidedish10.getter.service;
 
 import com.codesquad.sidedish10.getter.dto.BabChanItemResponseDto;
-import com.codesquad.sidedish10.getter.dto.BabChanItemRepository;
+import com.codesquad.sidedish10.getter.repository.BabChanItemRepository;
+import com.codesquad.sidedish10.getter.dto.BabChanSectionInfoDto;
+import com.codesquad.sidedish10.getter.repository.BabChanSectionInfoRepository;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetterService {
 
   private final BabChanItemRepository babChanItemRepository;
+  private final BabChanSectionInfoRepository babChanSectionInfoRepository;
 
   public GetterService(
-      BabChanItemRepository babChanItemRepository) {
+      BabChanItemRepository babChanItemRepository,
+      BabChanSectionInfoRepository babChanSectionInfoRepository) {
     this.babChanItemRepository = babChanItemRepository;
+    this.babChanSectionInfoRepository = babChanSectionInfoRepository;
   }
 
   public List<BabChanItemResponseDto> getBabChanItemLists() {
@@ -35,11 +42,23 @@ public class GetterService {
             element.setS_price(
                 babChanItemRepository.get_s_price_by_detail_hash(element.getDetail_hash()));
           }
-          if (babChanItemRepository.get_badge_by_detail_hash(element.getDetail_hash()) != null) {
-            element.setBadges(
-                babChanItemRepository.get_badge_by_detail_hash(element.getDetail_hash()));
+          if (babChanItemRepository.get_badge_title_by_detail_hash(element.getDetail_hash()) != null) {
+            List<Map<String, String>> nowList = new ArrayList<>();
+            babChanItemRepository.get_badge_title_by_detail_hash(element.getDetail_hash())
+                .forEach(title -> {
+                  HashMap<String, String> badgeMap = new HashMap<>();
+                  String thisColor = babChanItemRepository.get_badge_color_by_title(title);
+                  badgeMap.put("name", title);
+                  badgeMap.put("color", thisColor);
+                  nowList.add(badgeMap);
+                });
+            element.setBadges(nowList);
           }
         });
     return babChanItemResponseDtoList;
+  }
+
+  public List<BabChanSectionInfoDto> getBabChanMenuList() {
+    return babChanSectionInfoRepository.getInfoLists();
   }
 }
