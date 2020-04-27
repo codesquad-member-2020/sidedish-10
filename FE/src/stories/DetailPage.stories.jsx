2659496c814aa4de3f9c71bacd3dac33c21b21cb
sangwon21 <junothyme@gmail.com>
2020-04-27 12:30:52 +0900

    [#14] fix: 교체한 컴포넌트에 맞춰 스토리 수정
    why: 컴포넌트를 교체함에 따라, 이에 맞춰 스토리를 수정했다.

--- FE/src/stories/DetailPage.stories.jsx
+++ FE/src/stories/DetailPage.stories.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import luda from "../mockData/imgs/luda3.jpg";
 
 import DetailPage from "../components/DetailPage/DetailPage";
 
 storiesOf("DetailPage", module).addWithJSX("기본 설정", () => (
-  <DetailPage price={3800} />
+  <DetailPage
+    target={{ s_price: 3800, title: "루다 포토 카드", image: luda }}
+  />
 ));
