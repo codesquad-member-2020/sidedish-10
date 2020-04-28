dddd911657a5d0c9f0bee5f20c46765df97dae98
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-28 17:24:21 +0900

    config: attempt to solve not_found error with modifying redirection logic

--- BE/src/main/java/com/codesquad/sidedish10/getter/controller/LoginController.java
+++ BE/src/main/java/com/codesquad/sidedish10/getter/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
   }
 
   @GetMapping("/login/github/request")
-  public String GitHubLogin(@RequestParam("code") String code, HttpServletResponse res) {
+  public void GitHubLogin(@RequestParam("code") String code, HttpServletResponse res) {
 
     GitHubToken token = loginService.getAccessToken(code);
     User nowUser = loginService.getUserInfo(token);
@@ -37,9 +37,8 @@ public class LoginController {
 
     Cookie cookie = new Cookie("jwtToken", jwtToken);
     res.addCookie(cookie);
-    res.setStatus(HttpStatus.OK.value());
-
-    return "done";
+    res.setStatus(HttpStatus.SEE_OTHER.value());
+    res.setHeader("Location", "http://13.125.179.178");
   }
 
   @CrossOrigin("/**")
