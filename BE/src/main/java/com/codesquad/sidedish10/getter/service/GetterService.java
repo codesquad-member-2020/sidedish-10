7dfbf3ce4a455a1febb592e4d55dd6c41d75fbef
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-28 18:20:08 +0900

    feat: GetterService to be implemented with detail obtaining features.

--- BE/src/main/java/com/codesquad/sidedish10/getter/service/GetterService.java
+++ BE/src/main/java/com/codesquad/sidedish10/getter/service/GetterService.java
@@ -1,9 +1,13 @@
 package com.codesquad.sidedish10.getter.service;
 
+import com.codesquad.sidedish10.getter.domain.BabChanDetailReturnObject;
+import com.codesquad.sidedish10.getter.dto.BabChanDetailDto;
 import com.codesquad.sidedish10.getter.dto.BabChanItemResponseDto;
+import com.codesquad.sidedish10.getter.repository.BabChanDetailRepository;
 import com.codesquad.sidedish10.getter.repository.BabChanItemRepository;
 import com.codesquad.sidedish10.getter.dto.BabChanSectionInfoDto;
 import com.codesquad.sidedish10.getter.repository.BabChanSectionInfoRepository;
+import com.codesquad.sidedish10.parser.domain.BabChanDetailObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,12 +19,15 @@ public class GetterService {
 
   private final BabChanItemRepository babChanItemRepository;
   private final BabChanSectionInfoRepository babChanSectionInfoRepository;
+  private final BabChanDetailRepository babChanDetailRepository;
 
   public GetterService(
       BabChanItemRepository babChanItemRepository,
-      BabChanSectionInfoRepository babChanSectionInfoRepository) {
+      BabChanSectionInfoRepository babChanSectionInfoRepository,
+      BabChanDetailRepository babChanDetailRepository) {
     this.babChanItemRepository = babChanItemRepository;
     this.babChanSectionInfoRepository = babChanSectionInfoRepository;
+    this.babChanDetailRepository = babChanDetailRepository;
   }
 
   public List<BabChanItemResponseDto> getBabChanItemLists() {
@@ -42,7 +49,8 @@ public class GetterService {
             element.setS_price(
                 babChanItemRepository.get_s_price_by_detail_hash(element.getDetail_hash()));
           }
-          if (babChanItemRepository.get_badge_title_by_detail_hash(element.getDetail_hash()) != null) {
+          if (babChanItemRepository.get_badge_title_by_detail_hash(element.getDetail_hash())
+              != null) {
             List<Map<String, String>> nowList = new ArrayList<>();
             babChanItemRepository.get_badge_title_by_detail_hash(element.getDetail_hash())
                 .forEach(title -> {
@@ -60,5 +68,36 @@ public class GetterService {
 
   public List<BabChanSectionInfoDto> getBabChanMenuList() {
     return babChanSectionInfoRepository.getInfoLists();
+  }
+
+  public BabChanDetailDto getSpecificBabChanItemDetail(Long id) {
+
+    BabChanDetailReturnObject babChanDetailObject = babChanDetailRepository
+        .getBabChanItemDetailByHash(id);
+
+    BabChanDetailDto dto = BabChanDetailDto.create(babChanDetailObject.getId(), babChanDetailObject.getDetail_hash(),
+        babChanDetailObject.getTop_image(), babChanDetailObject.getProduct_description(),
+        babChanDetailObject.getPoint(), babChanDetailObject.getDelivery_info(),
+        babChanDetailObject.getDelivery_fee());
+
+    //TODO 진짜 최악의 코드인데 어떻게 해야 리팩토링할 수 있을 지 고민해본다.
+    if (babChanItemRepository.get_n_price_by_detail_hash(dto.getDetail_hash()) != null) {
+      dto.setN_price(
+          babChanItemRepository.get_n_price_by_detail_hash(dto.getDetail_hash()));
+    }
+    if (babChanItemRepository.get_s_price_by_detail_hash(dto.getDetail_hash()) != null) {
+      dto.setS_price(
+          babChanItemRepository.get_s_price_by_detail_hash(dto.getDetail_hash()));
+    }
+    if (babChanDetailRepository.getBabChanThumbImages(dto.getDetail_hash()) != null) {
+      dto.setThumb_images(
+          babChanDetailRepository.getBabChanThumbImages(dto.getDetail_hash()));
+    }
+    if (babChanDetailRepository.getBabChanDetailSectionByHash(dto.getDetail_hash()) != null) {
+      dto.setDetail_section(
+          babChanDetailRepository.getBabChanDetailSectionByHash(dto.getDetail_hash()));
+    }
+
+    return dto;
   }
 }
