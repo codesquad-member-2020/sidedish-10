41d1b0db455461fb67d5a4ea5c16aa1709d53268
1Consumption <hanseop95@gmail.com>
2020-04-28 20:57:53 +0900

    [#9] feat: 상세정보를 받을 모델 추가

---
+++ iOS/SideDish/SideDish/Model/DetailDish.swift
@@ -0,0 +1,13 @@
+//
+//  DetailDish.swift
+//  SideDish
+//
+//  Created by 신한섭 on 2020/04/28.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class DetailDish: NSObject {
+
+}
